e24d70ee091cf884f348b04c6253f4bf9131df07
shanon <shanonmambo@gmail.com>
2023-08-21 14:54:08 -0400

    more seo

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,36 +1,43 @@
-// next-seo.config.js
-
 const SEO = {
   defaultTitle: "Chroniclb | Premier Long Beach Dispensary",
-    description: "Chroniclb is a leading dispensary in Long Beach offering a wide range of cannabis products to suit every customer's needs. Visit us today!",
-    canonical: "https://ogchronic.com",
-    openGraph: {
-      type: 'website',
-      locale: 'en_US',
-      url: 'https://ogchronic.com',
-      title: "Chronic Long Beach | Premier Long Beach Dispensary",
-      description: "Chronic long beach is a leading dispensary in Southern california offering a wide range of cannabis products to suit every customer's needs. Visit us today!",
-      images: [
-        {
-          url: 'https://ogchronic.com/path_to_your_image.jpg',
-          alt: 'Chronic Long Beach Dispensary Image',
-        }
-      ],
-      site_name: 'Chroniclb',
-    },
-    twitter: {
-      handle: '@handle',  // You can fill this in with your Twitter handle if you have one
-      site: '@site',      // and the site's Twitter handle if it exists
-      cardType: 'summary_large_image',
-    },
-    additionalMetaTags: [
+  description: "Chroniclb is a leading dispensary in Long Beach offering a wide range of cannabis products to suit every customer's needs. Visit us today!",
+  canonical: "https://ogchronic.com",
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://ogchronic.com',
+    title: "Chronic Long Beach | Premier Long Beach Dispensary",
+    description: "Chronic long beach is a leading dispensary in Southern California offering a wide range of cannabis products. Visit us today!",
+    images: [
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        url: 'https://ogchronic.com/daily_deal.webp',
+        alt: 'Chronic Long Beach daily deals',
       },
       {
-        charSet: 'utf-8',
+        url: 'https://ogchronic.com/chronic_leaf.png',
+        alt: 'Chronic Long Beach leaf logo',
+      },
+      {
+        url: 'https://ogchronic.com/about/about_hero.webp',
+        alt: 'Chronic Long Beach about image',
       }
     ],
-  };
-  export default SEO;
+    site_name: 'Chronic long beach',
+  },
+  twitter: {
+    handle: '@ChroniclbHandle', // Replace with your actual handle or remove if you don't have one
+    site: '@ChroniclbSite',    // Replace with your site's actual handle or remove if you don't have one
+    cardType: 'summary_large_image',
+  },
+  additionalMetaTags: [
+    {
+      name: 'viewport',
+      content: 'width=device-width, initial-scale=1',
+    },
+    {
+      charSet: 'utf-8',
+    }
+  ],
+};
+
+export default SEO;
