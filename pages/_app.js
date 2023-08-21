2547e73d9c49ad6fbe3de0a7f8a6dc11c7c717eb
shanon <shanonmambo@gmail.com>
2023-08-21 14:30:35 -0400

    next seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,27 @@
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
 import Script from 'next/script'
+import SEO from './next-seo.config';
+import { DefaultSeo } from 'next-seo'; // <-- Import this
+
+
 import { Analytics } from '@vercel/analytics/react';
-
 import Head from 'next/head'
-
 
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
-  <Head>
-  <link rel="shortcut icon" href="/favicon/favicon.ico" />
-
-        <title>Chroniclb | </title>
-        <meta property="og:title" content="" key="title" />
-      </Head>     
-       <Component {...pageProps} />
-       <Analytics />
-       
-     
+      <Head>
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
       
+        {/* Other SEO Relevant Meta Tags */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+      </Head>
+      <DefaultSeo {...SEO} />
 
+      <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }
---
+++ pages/next-seo.config.js
@@ -0,0 +1,36 @@
+// next-seo.config.js
+
+export default {
+    defaultTitle: "Chroniclb | Premier Long Beach Dispensary",
+    description: "Chroniclb is a leading dispensary in Long Beach offering a wide range of cannabis products to suit every customer's needs. Visit us today!",
+    canonical: "https://ogchronic.com",
+    openGraph: {
+      type: 'website',
+      locale: 'en_US',
+      url: 'https://ogchronic.com',
+      title: "Chronic Long Beach | Premier Long Beach Dispensary",
+      description: "Chronic long beach is a leading dispensary in Southern california offering a wide range of cannabis products to suit every customer's needs. Visit us today!",
+      images: [
+        {
+          url: 'https://ogchronic.com/path_to_your_image.jpg',
+          alt: 'Chronic Long Beach Dispensary Image',
+        }
+      ],
+      site_name: 'Chroniclb',
+    },
+    twitter: {
+      handle: '@handle',  // You can fill this in with your Twitter handle if you have one
+      site: '@site',      // and the site's Twitter handle if it exists
+      cardType: 'summary_large_image',
+    },
+    additionalMetaTags: [
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        charSet: 'utf-8',
+      }
+    ],
+  };
+  
